ee52462b6f5a3aa201e042dd73c204d08c562989
Aditya <adityasingh00552@gmail.com>
2025-02-05 20:08:25 +0530

    Implemented function to fetch current weather data

--- JS/app.js
+++ JS/app.js
@@ -7,7 +7,23 @@ async function fetchWeatherData(cityName) {
   try {
     const response = await fetch(apiurl);
     const data = await response.json();
-    console.log("Weather Data: ", data);
+
+    if (data) {
+      const { main, name, weather, wind } = data;
+      let city = document.getElementById("city-name");
+      let humidity = document.getElementById("humidity");
+      let pressure = document.getElementById("pressure");
+      let temp = document.getElementById("temperature");
+      let desc = document.getElementById("weather-description");
+
+      city.innerText = `City: ${name}`;
+      humidity.innerText = `Humidity: ${main.humidity}%`;
+      pressure.innerText = `Pressure: ${main.pressure} hPa`;
+      temp.innerText = `Temperature: ${main.temp} K`;
+      desc.innerText = `Description: ${weather[0].description}`;
+    } else {
+      console.log("No Data Found!");
+    }
   } catch (err) {
     console.error("Error fetching Weather Data: ", err);
   }
